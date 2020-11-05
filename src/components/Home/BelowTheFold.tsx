a2a3a484eff6087a3f3f8684b65816b54011cbc4
Johnny Magrippis <j@magrippis.com>
2020-11-05 09:43:07 +0000

    point to twitch channel

--- src/components/Home/BelowTheFold.tsx
+++ src/components/Home/BelowTheFold.tsx
@@ -1,3 +1,12 @@
-const BelowTheFold = () => <>Will I be below the fold?</>
+const BelowTheFold = () => (
+  <div className="text-2xl">
+    We are <strong>livestreaming</strong> the creation of this AMAZING APP over
+    at{' '}
+    <a className="text-purple-500" href="https://twitch.tv/jmagrippis">
+      Twitch
+    </a>
+    ! Come join us!
+  </div>
+)
 
 export default BelowTheFold
