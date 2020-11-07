f3681ef392b2951f4c5697f8da0b73766b93f9ba
Johnny Magrippis <j@magrippis.com>
2020-11-07 21:20:35 +0000

    add error boundary

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,36 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { DefaultSeo } from 'next-seo'
-import { ReactQueryCacheProvider } from 'react-query'
+import { ReactQueryCacheProvider, useErrorResetBoundary } from 'react-query'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import { Header } from './Header'
 import { Footer } from './Footer'
 import { getSeoProps } from './getSeoProps'
 import { queryCache } from './queryCache'
+import { ErrorFallback } from './ErrorFallback'
 
-export const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-      />
-    </Head>
-    <DefaultSeo {...getSeoProps()} />
-    <ReactQueryCacheProvider queryCache={queryCache}>
-      <div className="w-full flex flex-grow flex-col items-center justify-between">
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </ReactQueryCacheProvider>
-  </>
-)
+export const App = ({ Component, pageProps }: AppProps) => {
+  const { reset } = useErrorResetBoundary()
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+      </Head>
+      <DefaultSeo {...getSeoProps()} />
+      <ErrorBoundary onReset={reset} fallbackRender={ErrorFallback}>
+        <ReactQueryCacheProvider queryCache={queryCache}>
+          <div className="w-full flex flex-grow flex-col items-center justify-between">
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
+          </div>
+        </ReactQueryCacheProvider>
+      </ErrorBoundary>
+    </>
+  )
+}
