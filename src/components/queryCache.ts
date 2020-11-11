f1343512c55e9ca373973c21a22c59f7d82dd4ad
Johnny Magrippis <j@magrippis.com>
2020-11-11 22:30:13 +0000

    specify a stale time

--- src/components/queryCache.ts
+++ src/components/queryCache.ts
@@ -1,3 +1,11 @@
 import { QueryCache } from 'react-query'
 
-export const queryCache = new QueryCache()
+const ONE_HOUR = 60 * 60 * 1000
+
+export const queryCache = new QueryCache({
+  defaultConfig: {
+    queries: {
+      staleTime: ONE_HOUR,
+    },
+  },
+})
