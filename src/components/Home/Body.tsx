d7a9783fc9df4042eb03cb3b8fdf0a92e74d283f
Johnny Magrippis <j@magrippis.com>
2020-11-11 20:17:20 +0000

    infinite lazy-load more events

--- src/components/Home/Body.tsx
+++ src/components/Home/Body.tsx
@@ -1,22 +1,35 @@
 import { QueryStatus } from 'react-query'
 import dynamic from 'next/dynamic'
+import { useInView } from 'react-intersection-observer'
 
 import { Loading } from 'components/Loading'
-import { useEvents } from 'marvelApi/useEvents'
+import { EventsResponse, useEvents } from 'marvelApi/useEvents'
 import { EventsList } from './EventsList/EventsList'
 
 import { ResponsiveHeading } from './ResponsiveTitle'
+import { MarvelEvent } from 'marvelApi/types'
+import { useEffect } from 'react'
 
 const BelowTheFold = dynamic(() => import('./BelowTheFold'))
 
+const reduceEventsResponse = (data: EventsResponse['data'][]): MarvelEvent[] =>
+  data.reduce<MarvelEvent[]>((acc, group) => [...acc, ...group.results], [])
+
 export const Body = () => {
-  const { data, status } = useEvents()
+  const { data, status, fetchMore, canFetchMore } = useEvents()
+  const { ref, inView } = useInView()
+
+  useEffect(() => {
+    if (inView && canFetchMore && status === QueryStatus.Success) {
+      fetchMore()
+    }
+  }, [inView, canFetchMore, status, fetchMore])
 
   return (
     <main className="w-full flex-grow container mb-4">
       <ResponsiveHeading className="mb-2" />
       {status === QueryStatus.Success ? (
-        <EventsList events={data.results} />
+        <EventsList events={reduceEventsResponse(data)} lastElementRef={ref} />
       ) : (
         <Loading />
       )}
--- src/components/Home/EventsList/EventsList.tsx
+++ src/components/Home/EventsList/EventsList.tsx
@@ -1,15 +1,17 @@
 import { MarvelEvent } from 'marvelApi/types'
+import { FC } from 'react'
 
 import { EventItem } from './EventItem'
 
 type Props = {
   events: MarvelEvent[]
+  lastElementRef: (node?: Element) => void
 }
 
-export const EventsList = ({ events }: Props) => (
+export const EventsList: FC<Props> = ({ events, lastElementRef }) => (
   <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
-    {events.map(({ id, title, description, thumbnail }) => (
-      <li key={id}>
+    {events.map(({ id, title, description, thumbnail }, i) => (
+      <li key={id} ref={i === events.length - 1 ? lastElementRef : undefined}>
         <EventItem
           title={title}
           thumbnail={thumbnail}
